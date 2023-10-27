443c339e100a04af64f24fcee8f9edb466bf8376
Tarsha <102608343+tarsha-simmons@users.noreply.github.com>
2023-10-26 19:56:53 -0500

    projects unfinished

---
+++ Slot machine JS project/project.js
@@ -0,0 +1,13 @@
+//1. deposit money
+//2. determine number of lines to bet
+//3. collect a bet amount
+//4. spin the slot machine
+//5. check if user won
+//6. give user their winnings
+//7. play again
+
+function deposit () {
+
+}
+
+deposit()
